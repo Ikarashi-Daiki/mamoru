c02cb8c3aad4eb30ec94bca94e27de3c11675af6
Ikarashi-Daiki <ec33083f@nagaoka-ct.ac.jp>
2022-03-13 23:44:51 +0900

    nannmosinai

--- main.js
+++ main.js
@@ -1,6 +1,34 @@
+import embed from 'vega-embed';
 import './style.css'
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+const spec = {
+  Object {
+    import React, {useRef, useEffect} from "react";
+import {Runtime, Inspector} from "@observablehq/runtime";
+import notebook from "a7ab1b8895dbf437";
+
+function Notebook() {
+  const speRef = useRef();
+
+  useEffect(() => {
+    const runtime = new Runtime();
+    runtime.module(notebook, name => {
+      if (name === "spe") return new Inspector(speRef.current);
+    });
+    return () => runtime.dispose();
+  }, []);
+
+  return (
+    <>
+      <div ref={speRef} />
+      <p>Credit: <a href="https://observablehq.com/d/a7ab1b8895dbf437">Untitled by ikarashi-daiki</a></p>
+    </>
+  );
+}
+
+export default Notebook;
+  }
+};
+
+embed("#app",spec);
+
